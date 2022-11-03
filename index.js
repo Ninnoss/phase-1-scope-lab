4fb6beca8322bbc40b4a1d90fbe21e50f94a1b63
Ninos Dinkha <ninos.dinkha97@gmail.com>
2022-11-03 15:01:33 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -20,23 +20,3 @@ function changeLeastFavoriteCustomer() {
   return LeastFavoriteCustomer = "New name";
 }
 
-
-
-// var customerName = "bob";
-// function upperCaseCustomerName() {
-//   customerName = customerName.toUpperCase();
-// }
-
-// function setBestCustomer() {
-//   bestCustomer = "not bob";
-// }
-
-// function overwriteBestCustomer() {
-//   bestCustomer = "maybe bob";
-// }
-
-// const leastFavoriteCustomer = "some initial value";
-
-// function changeLeastFavoriteCustomer() {
-//   leastFavoriteCustomer = "new name";
-// }
