d80d917f7aa3e0bc1a49a74d0eff2bcf73c352fc
Ninos Dinkha <ninos.dinkha97@gmail.com>
2022-11-03 15:00:32 +0300

    Assignment complete, all tests passed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = "maybe bob";
+}
+
+
+const LeastFavoriteCustomer = "Some initial name";
+
+function changeLeastFavoriteCustomer() {
+  return LeastFavoriteCustomer = "New name";
+}
+
+
+
+// var customerName = "bob";
+// function upperCaseCustomerName() {
+//   customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer() {
+//   bestCustomer = "not bob";
+// }
+
+// function overwriteBestCustomer() {
+//   bestCustomer = "maybe bob";
+// }
+
+// const leastFavoriteCustomer = "some initial value";
+
+// function changeLeastFavoriteCustomer() {
+//   leastFavoriteCustomer = "new name";
+// }
